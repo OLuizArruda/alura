2091e4f397855d6b597e44a6ae080c115e34196a
Canni <cannioficial@gmail.com>
2023-01-11 22:23:22 -0300

    11.01.2023

--- 02 - PHP/Avancando/listas.php
+++ 02 - PHP/Avancando/listas.php
@@ -3,6 +3,4 @@
 $listaIdades = [15, 30, 40, 80, 25];
 $umaIdade = $listaIdades[2];
 
-echo $umaIdade;
-
-/*echo "Ele tem $umaIdade anos";
+echo "Ele tem $umaIdade anos" . PHP_EOL;
---
+++ 02 - PHP/Avancando/loop-lista.php
@@ -0,0 +1,9 @@
+<?php
+
+$idadeList = [32, 24, 25, 45, 52, 78, 22];
+
+for ($i = 0; $i < count($idadeList); $i++){
+    echo "O valor da idade é $idadeList[$i] anos" . PHP_EOL;
+}
+
+echo "Terminei de Contar";
